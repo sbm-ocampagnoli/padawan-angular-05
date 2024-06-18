4d7d717047f8f96301901a7b4b39058a91baa8ed
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-18 10:35:19 -0300

    Criando um campo de busca.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,32 +3,33 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 
 @Component({
-  selector: 'app-listar-pensamento',
-  templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+    selector: 'app-listar-pensamento',
+    templateUrl: './listar-pensamento.component.html',
+    styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos: Pensamento[] = [];
-  paginaAtual: number = 1;
-  haMaisPensamentos: boolean = true;
+    listaPensamentos: Pensamento[] = [];
+    paginaAtual: number = 1;
+    haMaisPensamentos: boolean = true;
+    filtro: string = "testando filtro";
 
-  constructor(private service: PensamentoService) { }
+    constructor(private service: PensamentoService) { }
 
-  ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
-  }
+    ngOnInit(): void {
+        this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+            this.listaPensamentos = listaPensamentos
+        })
+    }
 
-  carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos.push(...listaPensamentos);
-        if(!listaPensamentos.length) {
-          this.haMaisPensamentos = false
-        }
-      })
-  }
+    carregarMaisPensamentos() {
+        this.service.listar(++this.paginaAtual)
+            .subscribe(listaPensamentos => {
+                this.listaPensamentos.push(...listaPensamentos);
+                if (!listaPensamentos.length) {
+                    this.haMaisPensamentos = false
+                }
+            })
+    }
 }
 

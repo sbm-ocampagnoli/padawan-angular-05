f6ff8c89109d290073fc1362de9d90ae3555c373
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-18 10:08:22 -0300

    Paginação e HttpParams

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,22 +1,21 @@
-import { PensamentoService } from './../pensamento.service';
-import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
+import { PensamentoService } from "./../pensamento.service";
+import { Component, OnInit } from "@angular/core";
+import { Pensamento } from "../pensamento";
 
 @Component({
-  selector: 'app-listar-pensamento',
-  templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+    selector: "app-listar-pensamento",
+    templateUrl: "./listar-pensamento.component.html",
+    styleUrls: ["./listar-pensamento.component.css"],
 })
 export class ListarPensamentoComponent implements OnInit {
+    listaPensamentos: Pensamento[] = [];
+    paginaAtual: number = 1;
 
-  listaPensamentos: Pensamento[] = [];
+    constructor(private service: PensamentoService) { }
 
-  constructor(private service: PensamentoService) { }
-
-  ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
-  }
+    ngOnInit(): void {
+        this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+            this.listaPensamentos = listaPensamentos;
+        });
+    }
 }
-

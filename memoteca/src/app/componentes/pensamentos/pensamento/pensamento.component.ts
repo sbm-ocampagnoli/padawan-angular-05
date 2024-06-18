c02ffa08f4932c62fee26b4c4a6890f7fe896c35
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-18 13:43:48 -0300

    Adicionando ícone de favorito.

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -2,29 +2,37 @@ import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pensamento',
-  templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+    selector: 'app-pensamento',
+    templateUrl: './pensamento.component.html',
+    styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent implements OnInit {
 
-  @Input() pensamento: Pensamento = {
-    id: 0,
-    conteudo: 'I love Angular',
-    autoria: 'Nay',
-    modelo: 'modelo3'
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  larguraPensamento(): string {
-    if(this.pensamento.conteudo.length >= 256) {
-      return 'pensamento-g'
+    @Input() pensamento: Pensamento = {
+        id: 0,
+        conteudo: 'I love Angular',
+        autoria: 'Nay',
+        modelo: 'modelo3',
+        favorito: false
     }
-    return 'pensamento-p'
-  }
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    larguraPensamento(): string {
+        if (this.pensamento.conteudo.length >= 256) {
+            return 'pensamento-g'
+        }
+        return 'pensamento-p'
+    }
+
+    mudarIconeFavorito(): string {
+        if (this.pensamento.favorito == false) {
+            return 'inativo';
+        }
+        return 'ativo';
+    }
 
 }

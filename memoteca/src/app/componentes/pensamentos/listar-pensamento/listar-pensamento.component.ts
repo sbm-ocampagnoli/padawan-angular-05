2b878f131d0e72ec1b546c4b258d75c556cdac0a
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-18 14:24:15 -0300

    Mostrando a lista de favoritos.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,42 +3,51 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 
 @Component({
-  selector: 'app-listar-pensamento',
-  templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+    selector: 'app-listar-pensamento',
+    templateUrl: './listar-pensamento.component.html',
+    styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos: Pensamento[] = [];
-  paginaAtual: number = 1;
-  haMaisPensamentos: boolean = true;
-  filtro: string = ''
+    listaPensamentos: Pensamento[] = [];
+    paginaAtual: number = 1;
+    haMaisPensamentos: boolean = true;
+    filtro: string = ''
 
-  constructor(private service: PensamentoService) { }
+    constructor(private service: PensamentoService) { }
 
-  ngOnInit(): void {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
-  }
+    ngOnInit(): void {
+        this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+            this.listaPensamentos = listaPensamentos
+        })
+    }
 
-  carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual, this.filtro)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos.push(...listaPensamentos);
-        if(!listaPensamentos.length) {
-          this.haMaisPensamentos = false
-        }
-      })
-  }
+    carregarMaisPensamentos() {
+        this.service.listar(++this.paginaAtual, this.filtro)
+            .subscribe(listaPensamentos => {
+                this.listaPensamentos.push(...listaPensamentos);
+                if (!listaPensamentos.length) {
+                    this.haMaisPensamentos = false
+                }
+            })
+    }
 
-  pesquisarPensamentos() {
-    this.haMaisPensamentos = true
-    this.paginaAtual = 1;
-    this.service.listar(this.paginaAtual, this.filtro)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos = listaPensamentos
-      })
-  }
+    pesquisarPensamentos() {
+        this.haMaisPensamentos = true
+        this.paginaAtual = 1;
+        this.service.listar(this.paginaAtual, this.filtro)
+            .subscribe(listaPensamentos => {
+                this.listaPensamentos = listaPensamentos
+            })
+    }
+
+    listarFavoritos() {
+        this.haMaisPensamentos = true;
+        this.paginaAtual = 1;
+        this.service.listarPensamentosFavoritos(this.paginaAtual, this.filtro)
+            .subscribe(listaPensamentosFavoritos => {
+                this.listaPensamentos = listaPensamentosFavoritos;
+            })
+    }
 }
 
